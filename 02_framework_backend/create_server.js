f95d161f708c5adb0d976109172e4dfd77565a9e
Ernesto Solano <ernesto.solano@alumnos.uneatlantico.es>
2020-10-31 16:51:55 +0100

    added pseudocode presented in last class

--- 02_framework_backend/create_server.js
+++ 02_framework_backend/create_server.js
@@ -1,14 +1,56 @@
 const events = require("events");
 const net = require("net");
 
-const createServer = (requestHandler) => {
-  // ... ✏️
+const server = net.createServer((socket) => {
+  console.log("new connection");
 
-  return {
-    listen: (portNumber) => {
-      // ... ✏️
+  let buffer = [];
+  socket.on("data", (data) => {
+    buffer.push(data);
+    console.log("data received");
+
+    // ...
+
+    if (!hasAllHeaders) {
+    return
     }
-  };
-};
+
+    // content-length ?
+
+    if (!isThereBody) {
+    // send response
+    return
+    }
+
+    // ...
+
+    if (!hasBody) {
+    return
+    }
+
+    // send response
+
+    const request = {
+    method = '...',
+    path = '...',
+    headers = {
+    //...
+    },
+    body = '...',
+    }
+
+    const response = {
+    send: () => { ... }
+    }
+
+    requestHandler(request, response)
+  });
+
+  socket.on("end", () => {
+    buffer.join();
+    console.log("connection ended");
+  });
+});
+
 
 module.exports = createServer;
